c2a1b7dcbb5c16e9f6389a3cb778c3a99ee51d66
Administrator <1839565349@qq.com>
2018-11-18 03:22:51 +0800

    基本配置 状态 控制

--- app/src/main/java/com/allyn/webview/MainActivity.java
+++ app/src/main/java/com/allyn/webview/MainActivity.java
@@ -1,20 +1,111 @@
 package com.allyn.webview;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView mWebview;
+    private MyWebView mWebview;
+    private ContentLoadingProgressBar mProSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mProSchedule = findViewById(R.id.pro_schedule);
         mWebview = findViewById(R.id.webview);
         mWebview.loadUrl("http://www.baidu.com");
+        //帮助WebView处理各种通知、请求事件
+        mWebview.setWebViewClient(new WebViewClient() {
 
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                mProSchedule.setVisibility(View.VISIBLE);
+                //开始
+                /**
+                 * 网页重定向时会执行多次
+                 */
+            }
+
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+                //网页加载成功
+
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+                //网址处理
+                /**
+                 * 可对指定网址进行拦截
+                 */
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                //网页加载失败
+                /**
+                 * 此回调中可进行自定义错误页面，
+                 * 遇到错误时示例代码:view.loadUrl("file://android_asset/error.html");
+                 */
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                mProSchedule.setVisibility(View.GONE);
+                //网页加载完成
+            }
+        });
+
+        //辅助WebView处理Javascript的对话框，网站图标，网站title，加载进度等
+        mWebview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                //网页加载进度
+                if (newProgress < 100) {
+                    mProSchedule.setProgress(newProgress);
+                }
+            }
+        });
+
+    }
+
+    //后退
+    public void clickReturn(View view) {
+        if (mWebview.canGoBack()) {
+            mWebview.goBack();
+        }
+    }
+
+    //前进
+    public void clickAhead(View view) {
+        if (mWebview.canGoForward()) {
+            mWebview.goForward();
+        }
+    }
+
+    //刷新
+    public void clickReload(View view) {
+        mWebview.reload();
+    }
+
+    //暂停
+    public void clickStop(View view) {
+        mWebview.stopLoading();
     }
 }
---
+++ app/src/main/java/com/allyn/webview/MyWebView.java
@@ -0,0 +1,60 @@
+package com.allyn.webview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+/**
+ * Created by allyn on 2018/11/18 0018.
+ */
+
+public class MyWebView extends WebView {
+
+    public MyWebView(Context context) {
+        super(context);
+    }
+
+    public MyWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        //基本配置
+        WebSettings webSettings = getSettings();
+        //支持缩放，默认为true。
+        webSettings.setSupportZoom(false);
+        //调整图片至适合webview的大小
+        webSettings.setUseWideViewPort(true);
+        // 缩放至屏幕的大小
+        webSettings.setLoadWithOverviewMode(true);
+        //设置默认编码
+        webSettings.setDefaultTextEncodingName("utf-8");
+        ////设置自动加载图片
+        webSettings.setLoadsImagesAutomatically(true);
+
+        //多窗口
+        webSettings.supportMultipleWindows();
+        //获取触摸焦点
+        requestFocusFromTouch();
+        //允许访问文件
+        webSettings.setAllowFileAccess(true);
+        //开启javascript
+        webSettings.setJavaScriptEnabled(true);
+        //支持通过JS打开新窗口
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        //提高渲染的优先级
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        //支持内容重新布局
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        //关闭webview中缓存
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
+    }
+
+}
